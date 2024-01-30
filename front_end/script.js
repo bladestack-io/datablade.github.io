65f94bc21f79ca076a44f0e7b76a2bb408449d26
Keifer Blandon <kblandon@bladestack.io>
2024-01-30 13:36:16 -0800

    update front end to communicate with API gateway

--- front_end/script.js
+++ front_end/script.js
@@ -1,16 +1,19 @@
 window.onload = function() {
     var userData = {
+        deviceId: 'some-unique-device-id', // Generate or define a unique ID
         browser: navigator.userAgent,
         screenSize: `${window.innerWidth} x ${window.innerHeight}`,
         os: navigator.platform
     };
 
-    displayData(userData);
+    fetch('https://55zyxaaql6.execute-api.us-east-1.amazonaws.com/prod', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData),
+    })
+    .then(response => response.json())
+    .then(data => console.log('Data sent successfully:', data))
+    .catch((error) => console.error('Error:', error));
 };
-
-function displayData(data) {
-    var infoDiv = document.getElementById('info');
-    infoDiv.innerHTML = `<p><strong>Browser (User Agent):</strong> ${data.browser}</p>
-                         <p><strong>Screen Size:</strong> ${data.screenSize}</p>
-                         <p><strong>Operating System:</strong> ${data.os}</p>`;
-}
