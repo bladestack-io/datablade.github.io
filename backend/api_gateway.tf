23fbca07bf425d453fc5787d3e5cfd443cec8158
Keifer Blandon <kblandon@bladestack.io>
2024-01-30 13:36:05 -0800

    added API gateway

---
+++ backend/api_gateway.tf
@@ -0,0 +1,37 @@
+resource "aws_api_gateway_rest_api" "api" {
+  name = "DataCaptureAPI"
+}
+
+resource "aws_api_gateway_resource" "api_resource" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "capture" # Change as needed
+}
+
+resource "aws_api_gateway_method" "api_method" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.api_resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.api_resource.id
+  http_method = aws_api_gateway_method.api_method.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.data_capture_function.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "api_deployment" {
+  depends_on = [aws_api_gateway_integration.lambda_integration]
+
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name  = "prod"
+}
+
+output "api_endpoint" {
+  value = "${aws_api_gateway_rest_api.api.execution_arn}/prod/capture"
+}
