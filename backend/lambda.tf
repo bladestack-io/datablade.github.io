51b2eaff3427803a5050a50fec92808d6bd893a4
Keifer Blandon <kblandon@bladestack.io>
2024-01-30 15:07:17 -0800

    updated permissions on lambda.tf

--- backend/lambda.tf
+++ backend/lambda.tf
@@ -1,5 +1,6 @@
+# AWS Lambda Execution Role
 resource "aws_iam_role" "lambda_role" {
-  name = "lambda_role"
+  name = "lambda_execution_role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -15,20 +16,26 @@ resource "aws_iam_role" "lambda_role" {
   })
 }
 
+# Attach the AWS managed basic execution role to the Lambda function
+# This allows the function to create and write to CloudWatch log groups
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# AWS Lambda Function
 resource "aws_lambda_function" "data_capture_function" {
   function_name = "DataCaptureFunction"
-
-  # Replace with the path to your Lambda deployment package
-  filename = "./lambda/lambda.zip"
-  handler  = "index.handler" # Update handler accordingly
-  role     = aws_iam_role.lambda_role.arn
-  runtime  = "nodejs14.x" # Ensure this matches your Lambda's runtime
+  filename      = "./lambda/lambda.zip"
+  handler       = "index.handler"
+  role          = aws_iam_role.lambda_role.arn
+  runtime       = "nodejs20.x"
 
   source_code_hash = filebase64sha256("./lambda/lambda.zip")
 }
 
-# Add necessary permissions and attach them to the role
-resource "aws_iam_role_policy_attachment" "lambda_policy" {
-  role       = aws_iam_role.lambda_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+# CloudWatch Log Group for the Lambda Function
+resource "aws_cloudwatch_log_group" "lambda_log_group" {
+  name              = "/aws/lambda/${aws_lambda_function.data_capture_function.function_name}"
+  retention_in_days = 14
 }
