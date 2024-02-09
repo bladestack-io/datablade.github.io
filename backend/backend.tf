437295558ff219e65e7bd012c57bb76f39b50e85
Keifer Blandon <kblandon@bladestack.io>
2024-02-09 08:31:53 -0800

    implemented remote backend configuration.

--- backend/backend.tf
+++ backend/backend.tf
@@ -1,4 +1,9 @@
 terraform {
-  backend "local" {
+    backend "s3" {
+    bucket         = "bsio-engineering-us-east-1-tf-state"
+    region         = "us-east-1"
+    key            = "aim-high-datablade-us-east-1.tfstate"
+    dynamodb_table = "bsio-engineering-us-east-1-tf-state-lock"
+    encrypt        = true
   }
 }
